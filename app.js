b8cd45e7d9a40a3a25ef13465753668349cd03da
Andreas Olsson <Olsson.Andreas@gmail.com>
2015-04-23 20:51:23 +0200

    Fix

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ angular.module('bowlingApp', [])
 		bowling.frames = [];
 		bowling.score = 0;
 		bowling.frameCount = 0;
+		
+		//public methods
+		bowling.addFrame = function() {
+			bowling.frameCount++;
+			if(bowling.third != "" && bowling.third != null) {
+				bowling.frames.push({"first": parseInt(bowling.first), "second": parseInt(bowling.second), "third": parseInt(bowling.third) });	
+			} else {
+				bowling.frames.push({"first": parseInt(bowling.first), "second": parseInt(bowling.second) });	
+			}
+			
+			bowling._clearFields();
 
-		bowling.clearFields = function() {
+			bowling._sendFramesToServer();
+		};
+
+		//private methods 
+		bowling._clearFields = function() {
 			bowling.first = "";
 			bowling.second = "";
 			bowling.third = "";
 		};
 
-		bowling.sendFramesToServer = function() {
+		bowling._sendFramesToServer = function() {
 			var jsonString = angular.toJson({"frames": bowling.frames});
+			console.log(jsonString);
 			$http({
 				url: 'http://localhost:49213/bowling',
 				dataType: 'json',
@@ -23,6 +39,7 @@ angular.module('bowlingApp', [])
 					}
 				}).
   				success(function(data, status, headers, config) {
+  					console.log(data);
     				bowling.score = data.score;
  				 }).
   				error(function(data, status, headers, config) {
@@ -30,18 +47,7 @@ angular.module('bowlingApp', [])
   				});
 		}
 
-		bowling.addFrame = function() {
-			bowling.frameCount++;
-			if(bowling.third != "") {
-				bowling.frames.push({"first": parseInt(bowling.first), "second": parseInt(bowling.second), "third": parseInt(bowling.third) });	
-			} else {
-				bowling.frames.push({"first": parseInt(bowling.first), "second": parseInt(bowling.second) });	
-			}
-			
-			bowling.clearFields();
-
-			bowling.sendFramesToServer();
-		};
+		
 	});
 
 
